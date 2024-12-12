4ee568ce318e3bcef6372ca82a2e5d2b06f5cad2
tfunk1030 <148044304+tfunk1030@users.noreply.github.com>
2024-12-12 13:25:34 -0500

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,44 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { ThemeProvider } from "./components/ui/theme-provider";
-import { Toaster } from "./components/ui/toaster";
-import { AuthProvider } from "./contexts/AuthContext";
-import { Login } from "./pages/Login";
-import { Register } from "./pages/Register";
-import { Layout } from "./components/Layout";
-import { ProtectedRoute } from "./components/ProtectedRoute";
-import { Home } from "./pages/Home";
-import { Repositories } from "./pages/Repositories";
-import { RepositoryDetail } from "./pages/RepositoryDetail";
-import { Settings } from "./pages/Settings";
-import axios from 'axios';
-
-axios.interceptors.request.use(config => {
-  const token = localStorage.getItem("token");
-  if (token && !config.headers.Authorization) {
-    config.headers.Authorization = `Token ${token}`;
-  }
-  return config;
-});
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from 'next-themes';
+import { Layout } from './components/Layout';
 
 function App() {
   return (
-    <AuthProvider>
-      <ThemeProvider defaultTheme="light" storageKey="ui-theme">
-        <Router>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/" element={<ProtectedRoute><Layout /></ProtectedRoute>}>
-              <Route index element={<Home />} />
-              <Route path="repositories" element={<Repositories />} />
-              <Route path="repositories/:id" element={<RepositoryDetail />} />
-              <Route path="settings" element={<Settings />} />
-            </Route>
-          </Routes>
-          <Toaster />
-        </Router>
-      </ThemeProvider>
-    </AuthProvider>
+    <ThemeProvider defaultTheme="light" storageKey="ui-theme">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
